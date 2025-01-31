1a0fc834c7743492651481e954fedbdd16ff6d92
AlexCanel <Alexcanelprofissional@gmail.com>
2025-01-31 14:42:48 -0300

    segundo commit

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let listaDeNumerosSorteados = [];
-let numeroLimite = 10;
+let numeroLimite = 100;
 let numeroSecreto = gerarNumeroAleatorio();
 console.log(numeroSecreto);
 let contador = 1;
@@ -48,7 +48,7 @@ function reiniciarJogo(){
 
 function exibirMensagenInicial(){   
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');      
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 100');      
 }
 
 function exibirTextoNaTela(tag, texto) {
@@ -61,7 +61,7 @@ function exibirTextoNaTela(tag, texto) {
     //     window.speechSynthesis.speak(utterance); 
     //  } else {
     //      console.log("Web Speech API não suportada neste navegador.");
-    //  }
+    // = }
  }
 
   function gerarNumeroAleatorio() {
